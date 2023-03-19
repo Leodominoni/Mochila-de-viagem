5a6b6d49da8f78c423d64153a378e2a1273018ec
Leonor <ldominoni@gmail.com>
2023-03-19 14:49:25 -0300

    find

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,60 @@
 const form = document.getElementById('novoItem');
+const lista = document.getElementById('lista');
+const itens  =  JSON.parse(localStorage.getItem('itens')) || [];
+
+itens.forEach((elemento)=>{
+    criaElemento(elemento);
+})
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
-    
-     //aqui teremos o conflito q recebe uma string
 
-    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
+   
+    const existe = itens.find(elemento => elemento.nome === nome.value) //buscar elemento e ver se existe um igual
+
+    const itemAtual = {
+        "nome": nome.value,
+        "quantidade": quantidade.value
+    }
+
+    if(existe){
+        itemAtual.id = existe.id;
+
+        atualizaElemento(itemAtual);
+    } else {
+        itemAtual.id = itens.length;
+
+        criaElemento(itemAtual);
+
+        itens.push(itemAtual);
+    }
+    
+    criaElemento(itemAtual);
+
+    itens.push(itemAtual); //add item no array
+
+    localStorage.setItem('itens', JSON.stringify(itens)); //transformar objeto em string
+
+    nome.value = '';
+    quantidade.value = '';
 });
 
-function criaElemento(nome, quantidade){
-    console.log(nome, quantidade);
-
+function criaElemento(item){
     const novoItem = document.createElement('li'); //Vai pegar um item da lista
     novoItem.classList.add('item'); //adicionar a classe item
 
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
-
+    numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    
+    novoItem.innerHTML += item.nome;
+    
+    lista.appendChild(novoItem);  
+}
 
-    console.log(novoItem)
+function atualizaElemento(item){
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
 }
